6ef2b3c81e94bdfa0c6374c9d3e3d306014891ae
Maanikya <maanikya.acharya@gmail.com>
2023-05-01 20:31:39 +0530

    CSS Updates

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -11,7 +11,17 @@ export const HeroContainer = styled.div`
     width: 100%;
     margin-top: -100px;
     position: absolute;
-    z-index: 0;
+    z-index: 1;
+
+    :before {
+        position: absolute;
+        top: 0;
+        left: 0;
+        bottom: 0;
+        background: linear-gradient(180deg, rgba(0, 0, 0, 0.2) 0%, rgba(0, 0, 0, 0.6) 100%),
+            linear-gradient(180deg, rgba(0, 0, 0, 0.2) 0%, transparent 100%),
+        z-index: 2;
+    }
 `;
 
 export const HeroBg = styled.div`
