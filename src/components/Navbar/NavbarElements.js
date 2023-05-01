51039dbf63745865027447c600ccb965b4c65624
Maanikya <maanikya.acharya@gmail.com>
2023-05-01 20:20:22 +0530

    Logo Positioning

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -36,7 +36,7 @@ export const NavLogo = styled(LinkR)`
     font-size: 1.5rem;
     display: flex;
     align-items: center;
-    margin-left: 24px;
+    margin-left: 12px;
     font-weight: bold;
     text-decoration: none;
 `;
